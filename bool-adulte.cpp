8c695df7b60dda4d4fdb6254f3f627c626c18985
VALT <102143297+Nathan-Vlt@users.noreply.github.com>
2022-04-13 17:49:16 +0200

    Add files via upload

---
+++ bool-adulte.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+bool adulte(1);
+int nbEnfants(2);
+
+
+
+if (adulte == true and nbEnfants <= 3)
+{
+    cout << "Vous etes une famille aimante " << endl;
+}
+else 
+cout << " cela est fort triste " << endl;
+}
---
+++ bool-char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{ 
+    int ageUtilisateur(16);
+    int nombreAmis(250);
+
+    double pi(3.14159);
+    
+    bool estMonAmi (true);
+
+    char lettre ('a');
+      
+    return 0;
+}
